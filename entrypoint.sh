09a9b7e49a2848540b6115b1e8621e30df3af59f
cristina-grosu <cristina.grosu@bigstep.com>
2018-10-24 17:23:18 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,109 +10,70 @@ apt-get install -y openssl
 # Generate self signed certificate
 openssl req -x509 -nodes  -days 365 -newkey rsa:1024 -keyout /tmp/jupyterhub.key -out /tmp/jupyterhub.crt
 
+# Give RW rights
+chmod 777 -R $NOTEBOOK_DIR
 # Generate JupyterHub config
 jupyterhub --generate-config
 
+# Configure JupyterHub
+sed "s/#c.JupyterHub.admin_access = False/c.JupyterHub.admin_access = True/" /jupyterhub_config.py >> /jupyterhub_config.py.tmp && \
+mv /jupyterhub_config.py.tmp /jupyterhub_config.py
 
+sed "s/#c.JupyterHub.admin_users = set()/c.JupyterHub.admin_users = {'admin'}/" /jupyterhub_config.py >> /jupyterhub_config.py.tmp && \
+mv /jupyterhub_config.py.tmp /jupyterhub_config.py
 
-if [ "$SPARK_UI_PORT" == "" ]; then
-  SPARK_UI_PORT=4040
-fi
+sed "s/#c.JupyterHub.default_url = ''/c.JupyterHub.default_url = ''/" /jupyterhub_config.py >> /jupyterhub_config.py.tmp && \
+mv /jupyterhub_config.py.tmp /jupyterhub_config.py
+ 
+sed "s/#c.JupyterHub.ssl_cert = ''/c.JupyterHub.ssl_cert = '/tmp/jupyterhub.crt'/" /jupyterhub_config.py >> /jupyterhub_config.py.tmp && \
+mv /jupyterhub_config.py.tmp /jupyterhub_config.py
+
+sed "s/#c.JupyterHub.ssl_key = ''/c.JupyterHub.ssl_key = '/tmp/jupyterhub.key'/" /jupyterhub_config.py >> /jupyterhub_config.py.tmp && \
+mv /jupyterhub_config.py.tmp /jupyterhub_config.py
 
 if [ "$NOTEBOOK_DIR" != "" ]; then
-	export ESCAPED_PERSISTENT_NB_DIR="${NOTEBOOK_DIR//\//\\/}"
-	
-	mkdir $NOTEBOOK_DIR/notebooks
-	cp /user/notebooks/* $NOTEBOOK_DIR/notebooks/
+	export ESCAPED_NOTEBOOK_DIR="${NOTEBOOK_DIR//\//\\/}"
 
-	sed "s/#c.NotebookApp.notebook_dir = ''/c.NotebookApp.notebook_dir = \'$ESCAPED_PERSISTENT_NB_DIR\/notebooks\'/" /root/.jupyter/jupyter_notebook_config.py >> /root/.jupyter/jupyter_notebook_config.py.tmp && \
-	mv /root/.jupyter/jupyter_notebook_config.py.tmp /root/.jupyter/jupyter_notebook_config.py
-	
+	sed "s/#c.Spawner.notebook_dir = ''/#c.Spawner.notebook_dir = \'$ESCAPED_NOTEBOOK_DIR\'/" /jupyterhub_config.py >> /jupyterhub_config.py.tmp && \
+	mv /jupyterhub_config.py.tmp /jupyterhub_config.py
 fi
 
-if [ "$MODE" == "" ]; then
-MODE=$1
+if [ "$USER1" != "" ]; then
+	adduser --quiet --disabled-password --shell /bin/bash --home $NOTEBOOK_DIR/$USER1 --gecos "User" $USER1
+	# set password
+	echo "$USER1:PASSWORD1" | chpasswd
 fi
 
-if [ "$MODE" == "jupyter" ]; then 
-	# Change the Home Icon 
-	sed "s/<i class=\"fa fa-home\"><\/i>/\/user/" /opt/conda/envs/python3/lib/python3.5/site-packages/notebook/templates/tree.html >> /opt/conda/envs/python3/lib/python3.5/site-packages/notebook/templates/tree.html.tmp
-	mv /opt/conda/envs/python3/lib/python3.5/site-packages/notebook/templates/tree.html.tmp /opt/conda/envs/python3/lib/python3.5/site-packages/notebook/templates/tree.html
-	
-	# export NOTEBOOK_PASSWORD=$(cat $NOTEBOOK_SECRETS_PATH/NOTEBOOK_PASSWORD)
+if [ "$USER2" != "" ]; then
+	adduser --quiet --disabled-password --shell /bin/bash --home $NOTEBOOK_DIR/$USER2 --gecos "User" $USER2
+	# set password
+	echo "$USER2:PASSWORD2" | chpasswd
+fi
 
-	pass=$(python /opt/password.py  $NOTEBOOK_PASSWORD)
-	sed "s/#c.NotebookApp.password = ''/c.NotebookApp.password = \'$pass\'/" /root/.jupyter/jupyter_notebook_config.py >> /root/.jupyter/jupyter_notebook_config.py.tmp && \
-	mv /root/.jupyter/jupyter_notebook_config.py.tmp /root/.jupyter/jupyter_notebook_config.py
+if [ "$USER3" != "" ]; then
+	adduser --quiet --disabled-password --shell /bin/bash --home $NOTEBOOK_DIR/$USER3 --gecos "User" $USER3
+	# set password
+	echo "$USER3:PASSWORD3" | chpasswd
+fi
 
-	if [ "$GIT_REPO_NAME" != "" ]; then
-		if [ "$GITHUB_COMMIT_DIR" == "" ]; then 
-			export GITHUB_COMMIT_DIR=/opt
-		fi
-		if [ "$GIT_PARENT_DIR" == "" ]; then 
-			export GIT_PARENT_DIR=$PERSISTENT_NB_DIR
-		fi
-		if [ "$GIT_BRANCH_NAME" == "" ]; then 
-			export GIT_BRANCH_NAME=master
-		fi
-		if [[ "$GIT_USER" != "" && "$GIT_EMAIL" != "" && "$GITHUB_ACCESS_TOKEN" != "" ]]; then 
-			if [ "$GIT_USER_UPSTREAM" == "" ]; then 
-				export GIT_USER_UPSTREAM=$GIT_USER
-			fi
-		
-			pip install git+https://github.com/sat28/githubcommit.git
-			jupyter serverextension enable --py githubcommit
-			jupyter nbextension install --py githubcommit
-			jupyter nbextension enable githubcommit --py
-		
-			cd $GITHUB_COMMIT_DIR && git clone https://github.com/sat28/githubcommit
-			rm -rf $GITHUB_COMMIT_DIR/githubcommit/env.sh
-			mv /opt/env.sh $GITHUB_COMMIT_DIR/githubcommit/
-		
-			sed "s/GITHUB_COMMIT_DIR=/GITHUB_COMMIT_DIR=$GITUB_COMMIT_DIR/" $GITHUB_COMMIT_DIR/githubcommit/env.sh >> $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp && \
-			mv $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp $GITHUB_COMMIT_DIR/githubcommit/env.sh
-		
-			sed "s/GIT_PARENT_DIR=/GIT_PARENT_DIR=$GIT_PARENT_DIR/" $GITHUB_COMMIT_DIR/githubcommit/env.sh >> $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp && \
-			mv $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp $GITHUB_COMMIT_DIR/githubcommit/env.sh
-		
-			sed "s/GIT_REPO_NAME=/GIT_REPO_NAME=$GIT_REPO_NAME/" $GITHUB_COMMIT_DIR/githubcommit/env.sh >> $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp && \
-			mv $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp $GITHUB_COMMIT_DIR/githubcommit/env.sh
-		
-			sed "s/GIT_BRANCH_NAME=/GIT_BRANCH_NAME=$GIT_BRANCH_NAME/" $GITHUB_COMMIT_DIR/githubcommit/env.sh >> $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp && \
-			mv $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp $GITHUB_COMMIT_DIR/githubcommit/env.sh
-		
-			sed "s/GIT_USER=/GIT_USER=$GIT_USER/" $GITHUB_COMMIT_DIR/githubcommit/env.sh >> $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp && \
-			mv $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp $GITHUB_COMMIT_DIR/githubcommit/env.sh
-		
-			sed "s/GIT_EMAIL=/GIT_EMAIL=$GIT_EMAIL/" $GITHUB_COMMIT_DIR/githubcommit/env.sh >> $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp && \
-			mv $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp $GITHUB_COMMIT_DIR/githubcommit/env.sh
-		
-			sed "s/GITHUB_ACCESS_TOKEN=/GITHUB_ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN/" $GITHUB_COMMIT_DIR/githubcommit/env.sh >> $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp && \
-			mv $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp $GITHUB_COMMIT_DIR/githubcommit/env.sh
-		
-			sed "s/GIT_USER_UPSTREAM=/GIT_USER_UPSTREAM=$GIT_USER_UPSTREAM/" $GITHUB_COMMIT_DIR/githubcommit/env.sh >> $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp && \
-			mv $GITHUB_COMMIT_DIR/githubcommit/env.sh.tmp $GITHUB_COMMIT_DIR/githubcommit/env.sh
-		
-			git config --global user.email "$GIT_EMAIL"
-			git config --global user.name "$GIT_USER"
-		
-			source $GITHUB_COMMIT_DIR/githubcommit/env.sh
-			cd $GIT_PARENT_DIR/$GIT_REPO_NAME && \
-			git config remote.master.url https://$GIT_USER:$GIT_ACCESS_TOKEN@github.com/$GIT_USER/$GIT_REPO_NAME.git
-		
-			rm -rf $CONDA_DIR/lib/python2.7/site-packages/githubcommit/handlers.py
-			mv /opt/env.sh $CONDA_DIR/lib/python3/site-packages/githubcommit/
-		fi
-	fi
-
-	#Install sparkmonitor extension
-	export SPARKMONITOR_UI_HOST=$SPARK_PUBLIC_DNS
-	export SPARKMONITOR_UI_PORT=$SPARK_UI_PORT
+if [ "$USER4" != "" ]; then
+	adduser --quiet --disabled-password --shell /bin/bash --home $NOTEBOOK_DIR/$USER4 --gecos "User" $USER4
+	# set password
+	echo "$USER4:PASSWORD4" | chpasswd
 fi
 
 
-if [[ "$MODE" == "jupyter" && "$SPARK_PUBLIC_DNS" == "" ]]; then 
-	jupyter notebook --ip=0.0.0.0 --log-level DEBUG --allow-root --NotebookApp.iopub_data_rate_limit=10000000000 
-else
-	jupyter notebook --ip=0.0.0.0 --log-level DEBUG --allow-root --NotebookApp.iopub_data_rate_limit=10000000000 --Spark.url="http://$SPARK_PUBLIC_DNS:$SPARK_UI_PORT"
+if [ "$USER5" != "" ]; then
+	adduser --quiet --disabled-password --shell /bin/bash --home $NOTEBOOK_DIR/$USER5 --gecos "User" $USER5
+	# set password
+	echo "$USER5:PASSWORD5" | chpasswd
 fi
+
+if [ "$USER6" != "" ]; then
+	adduser --quiet --disabled-password --shell /bin/bash --home $NOTEBOOK_DIR/$USER6 --gecos "User" $USER6
+	# set password
+	echo "$USER6:PASSWORD6" | chpasswd
+fi
+
+
+jupyterhub --ip=0.0.0.0 --log-level DEBUG 
